6dde1acd3ceb2462d09a97519a288d839e9a7952
Artur <artur.filipowski@trigar.pl>
2022-04-26 12:41:09 +0200

    CSRF3

--- link_app/views.py
+++ link_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
-@csrf_exempt
+@ensure_csrf_cookie
 def main_site(request):
     print("Dane\n",request.body)
     return render(request,'link_app/main.html')
